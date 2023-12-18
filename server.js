58b73e4b971582283478baba73955ecbf82bdba0
nikhil <nikhilmahato3337@gmail.com>
2023-12-18 14:39:02 +0530

    connecting mongo db and server setup

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("hello");
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.post("/", (req, res) => {
+  res.json(req.body);
+});
+
+const port = process.env.PORT || 5000;
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}..`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
